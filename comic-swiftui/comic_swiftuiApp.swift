03eadeb6a1e01ce0c18e6f96bd2bdaf43b01026b
vivalalova <vivalalova0@gmail.com>
2021-03-14 15:00:59 +0800

    Initial Commit

---
+++ comic-swiftui/comic_swiftuiApp.swift
@@ -0,0 +1,17 @@
+//
+//  comic_swiftuiApp.swift
+//  comic-swiftui
+//
+//  Created by Lova on 2021/3/14.
+//
+
+import SwiftUI
+
+@main
+struct comic_swiftuiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
